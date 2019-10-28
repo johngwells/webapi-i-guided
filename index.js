f418c053856ae5e5fec7396a5a152c3914bbe4b9
John Wells <molekewl2017@gmail.com>
2019-10-28 10:48:23 -0700

    basic node express api logic

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log('It works!')
+const express = require('express');
+
+const server = express(); // creates a server
+
+// request route handlers
+server.get('/', (req, res) => {
+  res.send('Hello Node 23');
+});
+
+// listen for requests in a particular port on localhost
+const port = 8000;
+server.listen(port, () => console.log('\n=== API on port 8000 ===\n'));
